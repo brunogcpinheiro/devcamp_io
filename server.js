024d13af8f15705e4df8077fca487b73c4630179
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2019-10-19 10:50:28 -0300

    unhandled rejections close server &  kill  process

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     "\x1b[4m",
@@ -39,3 +39,10 @@ app.listen(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT} `
   )
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
