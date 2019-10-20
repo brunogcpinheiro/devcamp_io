4b3c1c8b74715aba3366efa4e374209f31e98792
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2019-10-20 13:45:53 -0300

    Handle validation errors

--- middlewares/error.js
+++ middlewares/error.js
@@ -8,15 +8,21 @@ const errorHandler = (err, req, res, next) => {
   // Log to console for dev
   console.log(err);
 
-  // Mongoose bad ObjectId
+  // Mongoose Bad ObjectId
   if (err.name === "CastError") {
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
   }
 
-  // Mongoose duplicate key
+  // Mongoose Duplicate Key
   if (err.code === 11000) {
     const message = `Duplicate field value entered`;
+    error = new ErrorResponse(message, 400);
+  }
+
+  // Mongoose Validation Error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map(val => val.message);
     error = new ErrorResponse(message, 400);
   }
 
