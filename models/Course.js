d28e83ee54dd731bec65f893d2ef4346e9ecae8f
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2019-11-02 17:20:31 -0300

    create course model and course seeder

---
+++ models/Course.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose')
+
+const CourseSchema = new Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, 'Please add a course title']
+  },
+  description: {
+    type: String,
+    required: [true, 'Please add a description']
+  },
+  weeks: {
+    type: String,
+    required: [true, 'Please add a number of weeks']
+  },
+  tuition: {
+    type: Number,
+    required: [true, 'Please add a tuition cost']
+  },
+  minimumSkill: {
+    type: String,
+    required: [true, 'Please add a minimun skill'],
+    enum: ['beginner', 'intermediate', 'advanced']
+  },
+  scholarshipAvailable: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  bootcamp: {
+    type: Schema.ObjectId,
+    ref: 'Bootcamp',
+    required: true
+  }
+})
+
+module.exports = model('Course', CourseSchema)
