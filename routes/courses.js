70d43f1cc778f0c954fc3bd991323e83da3f678b
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2019-11-03 14:58:37 -0200

    add get all courses controller

---
+++ routes/courses.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const { getAllCourses } = require('../controllers/courses')
+
+const router = express.Router({ mergeParams: true })
+
+router.route('/').get(getAllCourses)
+
+module.exports = router
